526b2ae83cbd940eb280ef3d5996baa4f91f6e9e
William Reames <william.reames.19@cnu.edu>
2020-02-27 10:55:09 -0500

    Changed some output to work with the data in a more correct manner. Fixed a bug preventing all data from being read in.

--- README.md
+++ README.md
@@ -3,31 +3,29 @@
 
 This program was made to parse through data on https://osint.bambenekconsulting.com/feeds/ and output relevant
 information. This website contains intel feeds for what appear to be various botnets. Each individual botnet gets their
-own set of files that contain information about the domains, IP Addresses that are produced from the domains, Domain
-Name Service (DNS) Hosts that the IPs use to connect to a new set of IPs, and IPs produced by the DNS Hosts. It is
-possible that the IPs produced by the DNS Host are the IPs of C&C Servers in an IRC botnet, or another bot in a P2P
-botnet. All of this information is contained inside the "master" file, which is the file my program is primarily
-concerned with. My program parses through all the information and pieces together potential pieces of important
-information by using the domains, IP Addresses, and IPs produced by the DNS Hosts of each individual bot in the botnet.
+own set of files that contain information about the domains, IP Addresses created through the use of a Name Service
+(NS) Server, the NS Host domain, and the NS IP Address. It is possible that the IPs produced by the NS are the IPs of
+Command and Control (C&C) Servers in an IRC botnet, or another form of a botnet which uses a C&C server. All of this
+information is contained inside the "master" file, which is the file my program is primarily concerned with. My program
+parses through all the information and pieces together potential pieces of important information by using the domains,
+IP Addresses, and IPs produced by the DNS Hosts of each individual bot in the botnet.
 
 **What my program is finding:**
 
 When parsing through the master files, my program looks for 3 main things to output. Firstly, it looks to see how many
-IP Addresses repeat with a different domain. I'm not entirely sure what this indicates, but I found it interesting.
-Secondly, it finds how many different IPs point to the same IP created by the DNS Host. Depending on what botnet is
-being parsed, it could either be a P2P botnet, or a botnet similar in structure to an IRC botnet in which there is a
-central C&C server. In the P2P system, seeing multiple IPs being directed to a single IP could indicate that the IP
-created by the DNS Host is actually another bot inside the botnet. As for an IRC system, or similar, this could
-indicate that the IP created by the DNS Host is one of the C&C servers, as there are multiple bots in the botnet that
-are accessing it. Through the knowledge of this information, we may be able to find the IP Address of either the C&C
-server or another bot in the P2P system. Thirdly, my program finds the location of the IP of each bot in the botnet.
-This can help to understand where a majority of the bots are located, as well as tell us how widespread the botnet is.
+IP Addresses repeat with a different domain. As these IPs are likely the IPs associated with a C&C server, this could
+tell us which servers are being used more often than others, and which ones are a higher priority to take down.
+Secondly, it finds how many different C&Cs are using the same NS Server. This is split into two main parts, C&C servers
+that share similar NS Host domains, and C&C Servers that share similar NS IP Addresses. This could help us understand
+which NS Servers are being used more frequently than others, and would allow us to know where most of the C&C data is
+being sent. Thirdly, my program finds the location of each C&C IP that is used inside the file. This can help to
+understand where a majority of the servers are located, as well as tell us how widespread the botnet is.
 
 Furthermore, my program also gives an option to parse through the website's "C2 IP Feed". This intel feed contains a
-list of every IP that is used across all of the botnets. Using this, my program finds how many times an IP repeats
-between various botnets, as well as which botnets are using the same IP for a bot. This is interesting as it may
-indicate that certain botnet systems may be working in tandem with each other, or that there is a single system
-infected with multiple botnets at the same time.
+list of every IP that is used across all of the servers. Using this, my program finds how many times an IP repeats
+between various servers, as well as which botnets are using the same IP for a server. This is interesting as it may
+indicate that certain botnet systems may be working in tandem with each other, or that there is a single system that is
+being used for multiple C&C servers at the same time.
 
 **Using my program:**
 
--- tests/test_parser.py
+++ tests/test_parser.py
@@ -10,6 +10,7 @@ class TestParser(unittest.TestCase):
         self.file_path1 = '../test_data/dircrypt-master.csv'
         self.file_path2 = '../test_data/bedep-master.csv'
         self.file_path3 = '../test_data/bamital-master.csv'
+        self.file_path4 = '../test_data/simda-master.csv'
 
         # Creates a testing instance off of the first file path
         self.parser1 = botnet_parser.BotnetParser()
@@ -22,6 +23,9 @@ class TestParser(unittest.TestCase):
         # Creates a testing instance off of the third file path
         self.parser3 = botnet_parser.BotnetParser()
         self.parser3._master_data_from_file(self.file_path3)
+
+        self.parser4 = botnet_parser.BotnetParser()
+        self.parser4._master_data_from_file(self.file_path4)
 
     def test_correct_domains(self):
         correct_domains1 = {'kcubcfuhwwn.com', 'lscyqrjofqmtn.com', 'lxpcmncky.com', 'ojglbpuhj.com',
@@ -43,19 +47,22 @@ class TestParser(unittest.TestCase):
                              correct_domains3, self.parser3.domains))
 
     def test_correct_ips(self):
-        correct_ips1 = {'169.50.13.61', '164.155.204.36', '193.146.253.35'}
-        correct_ips2 = {'173.231.184.117', '52.0.217.44', '162.217.99.136', '173.231.184.57', '72.26.218.84',
-                        '72.26.218.74', '72.251.233.252', '107.6.74.79'}
+        correct_ips1 = ['169.50.13.61', '164.155.204.36', '193.146.253.35']
+        correct_ips2 = ['173.231.184.117', '52.0.217.44', '162.217.99.136', '173.231.184.57', '72.26.218.84',
+                        '72.26.218.74', '72.251.233.252', '107.6.74.79']
         correct_ips3 = []
-        self.assertEqual(correct_ips1, self.parser1.bots.keys(),
-                         msg='Parser should have {} as its set of ips but instead has {}.'.format(
-                             correct_ips1, self.parser1.bots))
-        self.assertEqual(correct_ips2, self.parser2.bots.keys(),
-                         msg='Parser should have {} as its set of ips but instead has {}.'.format(
-                             correct_ips2, self.parser2.bots))
+        correct_ips4 = ['193.41.64.176', '141.105.126.87']  # Only uses the first and last IP
+        self.assertEqual(correct_ips1, self.parser1.ips,
+                         msg='Parser should have {} as its list of ips but instead has {}.'.format(
+                             correct_ips1, self.parser1.ips))
+        self.assertEqual(correct_ips2, self.parser2.ips,
+                         msg='Parser should have {} as its list of ips but instead has {}.'.format(
+                             correct_ips2, self.parser2.ips))
         self.assertEqual(correct_ips3, self.parser3.ips,
-                         msg='Parser should have {} as its set of ips but instead has {}.'.format(
-                             correct_ips3, self.parser3.bots))
+                         msg='Parser should have {} as its list of ips but instead has {}.'.format(
+                             correct_ips3, self.parser3.ips))
+        self.assertEqual(correct_ips4[0], self.parser4.ips[0], msg='The first IP in parser is incorrect')
+        self.assertEqual(correct_ips4[-1], self.parser4.ips[-1], msg='The last IP in parser is incorrect')
 
     def test_correct_repeating_ips(self):
         correct_repeating_ips1 = {'169.50.13.61': 4, '193.146.253.35': 4}
@@ -71,36 +78,56 @@ class TestParser(unittest.TestCase):
                          msg='Parser should have {} as its set of repeating ips but instead has {}.'.format(
                              correct_repeating_ips3, self.parser3.repeating_ips_count))
 
-    def test_correct_multiple_pointers(self):
-        correct_multiple_pointers1 = {}
-        correct_multiple_pointers3 = {}
-        self.parser1.find_similar_dns_ips()
-        self.assertEqual(correct_multiple_pointers1, self.parser1.multiple_pointers,
+    def test_correct_multiple_ip_uses1(self):
+        correct_multiple_ip_uses1 = {}
+        correct_multiple_ip_uses3 = {}
+        self.parser1.find_similar_dns_info()
+        self.assertEqual(correct_multiple_ip_uses1, self.parser1.multiple_ip_uses,
                          msg='Parser should have {} as the dictionary for multiple_pointers, '
-                             'but instead has {}.'.format(correct_multiple_pointers1,
-                                                          self.parser1.multiple_pointers))
-        self.parser3.find_similar_dns_ips()
-        self.assertEqual(correct_multiple_pointers3, self.parser3.multiple_pointers,
+                             'but instead has {}.'.format(correct_multiple_ip_uses1,
+                                                          self.parser1.multiple_ip_uses))
+        self.parser3.find_similar_dns_info()
+        self.assertEqual(correct_multiple_ip_uses3, self.parser3.multiple_ip_uses,
                          msg='Parser should have {} as the dictionary for multiple_pointers, '
-                             'but instead has {}.'.format(correct_multiple_pointers3,
-                                                          self.parser3.multiple_pointers))
+                             'but instead has {}.'.format(correct_multiple_ip_uses3,
+                                                          self.parser3.multiple_ip_uses))
 
-    def test_correct_multiple_pointers2(self):
+    def test_correct_multiple_ip_uses2(self):
         # Since the lists for this test case were longer than others, I only created tests for two keys
-        correct_multiple_pointers2 = {'54.221.105.86': {'173.231.184.117', '162.217.99.136', '173.231.184.57',
-                                                        '72.26.218.84', '72.26.218.74', '72.26.218.74',
-                                                        '72.251.233.252', '107.6.74.79'},
-                                      '3.87.22.163': {'162.217.99.136', '173.231.184.57', '72.26.218.84',
-                                                      '107.6.74.79'}}
-        self.parser2.find_similar_dns_ips()
-        keys = correct_multiple_pointers2.keys()
+        correct_multiple_ip_uses2 = {'54.221.105.86': {'173.231.184.117', '162.217.99.136', '173.231.184.57',
+                                                       '72.26.218.84', '72.26.218.74', '72.26.218.74',
+                                                       '72.251.233.252', '107.6.74.79'},
+                                     '3.87.22.163': {'162.217.99.136', '173.231.184.57', '72.26.218.84',
+                                                     '107.6.74.79'}}
+        correct_multiple_ip_uses4 = {'195.206.121.10': {'46.30.215.194', '77.111.240.32', '77.111.240.52',
+                                                        '91.212.28.29', '46.30.215.126'},
+                                     '81.2.216.125': {'62.149.128.151', '62.149.128.154', '62.149.128.157',
+                                                      '62.149.128.160', '62.149.128.163', '62.149.128.166',
+                                                      '62.149.128.72', '62.149.128.74', '89.46.108.57',
+                                                      '31.11.32.144'}}
+        self.parser2.find_similar_dns_info()
+        keys = correct_multiple_ip_uses2.keys()
         for key in keys:
-            self.assertTrue(self.parser2.multiple_pointers[key])  # Tests if the correct keys exist in the dictionary
-            self.assertEqual(len(correct_multiple_pointers2[key]), len(self.parser2.multiple_pointers[key]),
-                             msg="The length of multiple_pointers with the key {} is meant to be {} "
-                                 "but it is actually {}".format(key, len(correct_multiple_pointers2[key]),
-                                                                len(self.parser2.multiple_pointers[key])))
-            self.assertEqual(correct_multiple_pointers2[key], self.parser2.multiple_pointers[key])
+            self.assertTrue(self.parser2.multiple_ip_uses[key])  # Tests if the correct keys exist in the dictionary
+            self.assertEqual(correct_multiple_ip_uses2[key], self.parser2.multiple_ip_uses[key])
+        self.parser4.find_similar_dns_info()
+        keys = correct_multiple_ip_uses4.keys()
+        for key in keys:
+            self.assertTrue(self.parser4.multiple_ip_uses[key])  # Tests if the correct keys exist in the dictionary
+            self.assertEqual(correct_multiple_ip_uses4[key], self.parser4.multiple_ip_uses[key])
+
+    def test_correct_multiple_host_uses(self):
+        correct_multiple_host_uses = {'ns01.one.com': {'46.30.215.194', '77.111.240.32', '77.111.240.52',
+                                                       '91.212.28.29', '46.30.215.126'},
+                                      'dns2.technorail.com': {'62.149.128.151', '62.149.128.154', '62.149.128.157',
+                                                              '62.149.128.160', '62.149.128.163', '62.149.128.166',
+                                                              '62.149.128.72', '62.149.128.74', '89.46.108.57',
+                                                              '31.11.32.144'}}
+        self.parser4.find_similar_dns_info()
+        keys = correct_multiple_host_uses.keys()
+        for key in keys:
+            self.assertTrue(self.parser4.multiple_host_uses[key])  # Tests if the correct keys exist in the dictionary
+            self.assertEqual(correct_multiple_host_uses[key], self.parser4.multiple_host_uses[key])
 
     def test_correct_locations(self):
         correct_locations1 = {'US': 2, 'ES': 1}
